990679f6815f76b3e6b53d1d1afd1d8a799c60b1
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2017-12-04 14:07:46 +0100

    Day 4 star 2

--- day4.js
+++ day4.js
@@ -1,12 +1,21 @@
 function main1(input) {
 	let reg = /\b(\w+)\b.*\b\1\b/g;
-	let array = input.split('\n');
-	return array.reduce((acc, line) => {
-		if(line.match(reg) === null) {
-			return acc+1;
-		}
-		return acc;
-	}, 0);
+	return input.split('\n')
+		.reduce((acc, line) => {
+			if (line.match(reg) === null) {
+				return acc + 1;
+			}
+			return acc;
+		}, 0);
 }
 
 main1(document.body.firstChild.innerText.trim());
+
+function main2(input) {
+	let reg;
+	return input.split('\n')
+		.reduce((acc, line) => {
+			line.split(' ');
+			return acc;
+		}, 0);
+}
