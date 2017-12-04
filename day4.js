16c88f991b25b9196fed710672716f7b30a24b1f
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2017-12-04 14:10:54 +0100

    Remove useless sort call

--- day4.js
+++ day4.js
@@ -15,7 +15,37 @@ function main2(input) {
 	let reg;
 	return input.split('\n')
 		.reduce((acc, line) => {
-			line.split(' ');
-			return acc;
+			let letterCounts = line.split(' ')
+				.map((current) => {
+					let letterMap = {};
+					for (let i = 0; current[i]; i++) {
+						letterMap[current[i]] = letterMap[current[i]] ? letterMap[current[i]] + 1 : 1;
+					}
+					return letterMap;
+				});
+			for (let i = 0; letterCounts[i]; i++) {
+				for (let j = i + 1; letterCounts[j]; j++) {
+					if (compare(letterCounts[i], letterCounts[j])) {
+						return acc;
+					}
+				}
+			}
+			return acc + 1;
 		}, 0);
 }
+
+function compare(obj1, obj2) {
+	let key1 = Object.keys(obj1),
+		key2 = Object.keys(obj2);
+	if (key1.length !== key2.length) {
+		return false
+	}
+	for (let i = 0; key1[i]; i++) {
+		if (obj1[key1[i]] !== obj2[key1[i]]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+main2(document.body.firstChild.innerText.trim());
