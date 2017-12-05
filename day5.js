e41b07957d42966bb2c13d87add1e1cb5d52eb91
Xavier FILIPPI <xavier.filippi.prestataire@sfr.com>
2017-12-05 11:38:19 +0100

    Both stars of day5

--- day5.js
+++ day5.js
@@ -1,14 +1,15 @@
-.split('\n').map(Number);
-
-function main(input) {
-	let array = input.split('\n').map(Number), i = 0, count = 0;
-	while(array[i] !== void 0) {
+const input = document.body.firstChild.innerText.trim();
+function main(input, increment) {
+	let array = input.split('\n').map(Number),
+		i = 0,
+		count = 0;
+	while (array[i] !== void 0) {
 		let j = i + array[i];
-		array[i]++;
+		array[i] = increment(array[i]);
 		i = j;
 		count++;
 	}
 	return count;
 }
-
-main(document.body.firstChild.innerText.trim());
+main(input, (i) => i + 1);
+main(input, (i) => (i >= 3) ? i - 1 : i + 1);
