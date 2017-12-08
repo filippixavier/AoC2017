04239300e541932d48a74aa7babe4dffa5b54fe8
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2017-12-08 12:01:36 +0100

    Second star for day8

--- day8.js
+++ day8.js
@@ -1,6 +1,7 @@
 function main (input) {
-	let regx = /(\w+) (\w+) (\d+) if (\w+) (.+) (\d+)/;
+	let regx = /(\w+) (\w+) (-?\d+) if (\w+) (.+) (-?\d+)/;
 	let registries = {};
+	let maxValue = 0;
 	let opcodes = {
 		'>' : (reg, value) => reg > value,
 		'<' : (reg, value) => reg < value,
@@ -17,17 +18,14 @@ function main (input) {
 		registries[operation[0]] = registries[operation[0]] || 0;
 		registries[operation[3]] = registries[operation[3]] || 0;
 
-		if (opcodes[operation[4]](registries[operation[4]], +operation[5])) {
-			registries[operation[0]] = opcodes[operation[1]](registries[operation[0]], +operation[2])
+		if (opcodes[operation[4]](registries[operation[3]], +operation[5])) {
+			registries[operation[0]] = opcodes[operation[1]](registries[operation[0]], +operation[2]);
+			maxValue = Math.max(maxValue, registries[operation[0]]);
 		}
 	});
 
-	return Math.max(Object.values(registries));
+	console.log(maxValue);
+	return Math.max(...Object.values(registries));
 }
 
-var test = `b inc 5 if a > 1
-a inc 1 if b < 5
-c dec -10 if a >= 1
-c inc -20 if c == 10`;
-
-main(test); 
+main(main(document.body.firstChild.innerText.trim())); 
