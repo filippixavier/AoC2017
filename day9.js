5b7217e8bef20d0bdf18e2991e5527399606a1b0
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2017-12-09 16:15:25 +0100

    Day 9 second star

--- day9.js
+++ day9.js
@@ -3,13 +3,19 @@ function main(input) {
     let garbage = false;
     let groupScore = 1;
     let totalScore = 0;
+    let garbageCollected = 0;
+    let garbageStartIndex;
+    let notCounted = 0;
     let char;
     while(char = reg.exec(input)) {
         if(garbage) {
             if(char[0] === '>') {
                 garbage = false;
+                garbageCollected += reg.lastIndex - garbageStartIndex - 1 - notCounted;
+                notCounted = 0;
             } else if (char[0] === '!') {
                 reg.lastIndex++;
+                notCounted += 2;
             }
         } else {
             if(char[0] === '{') {
@@ -17,12 +23,13 @@ function main(input) {
                 groupScore++;
             } else if(char[0] === '<') {
                 garbage = true;
+                garbageStartIndex = reg.lastIndex;
             } else if(char[0] === '}') {
                 groupScore--;
             }
         }
     }
-    return totalScore
+    return {totalScore, garbageCollected}
 }
 
 main(document.body.innerText);
