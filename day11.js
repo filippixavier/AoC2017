065811155655015c86f85b73104ebbdc497af50c
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2017-12-11 14:13:36 +0100

    Day 11 second star

--- day11.js
+++ day11.js
@@ -1,25 +1,28 @@
 function main(input) {
-	let n = /n\b/g, ne = /ne/g, se = /se/g, s = /s\b/g, sw = /sw/g, nw = /nw/g;
-	let steps = [input.match(n), input.match(ne), input.match(se), input.match(s), input.match(sw), input.match(nw)].map((elem) => elem ? elem.length : 0);
-	for(let i = 0; i < steps.length/2; i++) {
-		let opposite = (i+3)%6;
-		let a = steps[i] - steps[opposite];
-		steps[i] = steps[opposite] = 0;
-		if(a > 0) {
-			steps[i] = a;
-		} else {
-			steps[opposite] = -a;
+	let max = 0;
+	let value = input.split(',').map((step) => {
+		switch (step) {
+		case 'n':
+			return [1, 0];
+		case 'ne':
+			return [0.5, 1];
+		case 'nw':
+			return [0.5, -1];
+		case 's':
+			return [-1, 0];
+		case 'se':
+			return [-0.5, 1];
+		case 'sw':
+			return [-0.5, -1];
 		}
-	}
-	for(let i = 0; i < steps.length; i++) {
-		let left = i-1 < 0? steps.length - 1 : i - 1;
-		let right = (i+1) % steps.length;
-		let min = Math.min(steps[left], steps[right]);
-		steps[i] += min;
-		steps[left] -= min; steps[right] -= min;
-	}
-
-	return steps.reduce((acc, current) => acc + current, 0);
+	}).reduce((acc, current) => {
+		let temp = [acc[0] + current[0], acc[1] + current[1]];
+		let dist = Math.abs(temp[0]) - (Math.abs(temp[1]) * 0.5) + Math.abs(temp[1]);
+		max = (dist > max)? dist : max;
+		return temp;
+	}, [0,0]).map(val => Math.abs(val));
+	console.log(max);
+	return value[0] - (value[1] * 0.5) + value[1];
 }
 
 main(document.body.innerText.trim());
