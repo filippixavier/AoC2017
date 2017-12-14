2ec4c8ac74877588ff3b50d5f1312cc7676fe2e4
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2017-12-14 13:19:15 +0100

    Optimize solution

--- day13.js
+++ day13.js
@@ -1,40 +1,34 @@
-var delay = 10;
+var delay = -1;
 var text = document.body.innerText.trim();
+var ranges = [];
+text.split('\n').forEach((elem) => {
+	let convert = elem.match(/\d+/g).map(Number);
+	ranges.push([convert[0], convert[1]]);
+});
 
-function main(input, delay) {
-	let del = delay || 0;
-	let ranges = [],
-		severity = 0;
-	input.split('\n').forEach((elem) => {
-		let convert = elem.match(/\d+/g).map(Number);
-		ranges[convert[0]] = convert[1];
-	});
-	for (let i = 0; i < ranges.length; i++) {
-		if (ranges[i] && !((i + del) % ((ranges[i] - 1) * 2))) {
-			severity += i * ranges[i];
+function main() {
+	let severity = 0;
+	for (let range of ranges) {
+		let [index, length] = range;
+		if (!(index % ((length - 1) * 2))) {
+			severity += index * length;
 		}
 	}
 
 	return severity;
 }
 
-function main2(input, delay) {
-	let del = delay || 0;
-	let ranges = [],
-		severity = 0;
-	input.split('\n').forEach((elem) => {
-		let convert = elem.match(/\d+/g).map(Number);
-		ranges[convert[0]] = convert[1];
-	});
-	for (let i = 0; i < ranges.length; i++) {
-		if (ranges[i] && !((i + delay) % ((ranges[i] - 1) * 2))) {
+function main2(delay) {
+	let severity = 0;
+	for (let range of ranges) {
+		let [index, length] = range;
+		if (!(index % ((length - 1) * 2))) {
 			return false;
 		}
 	}
-
 	return true;
 }
 
 
-main(text);
-while(main2(text, delay) !== 0) delay++;
+main();
+while (main2(++delay));
