aaa74102da56c09511b767d52926b39f4713c1ac
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2017-12-15 17:20:38 +0100

    Day 15 both stars

--- Day15.js
+++ Day15.js
@@ -1,10 +1,12 @@
-function* generator(seed, factor) {
+function* generator(seed, factor, check) {
 	let fact = factor;
 	let input = seed;
 	let divider = 2147483647;
 	while(true) {
 		input =  (input * factor) % divider;
-		yield input;
+		if(!(input % check)) {
+			yield input;
+		}
 	}
 }
 
@@ -13,13 +15,18 @@ function judge(valueA, valueB) {
 	return (valueA & mask) === (valueB & mask);
 }
 
-function main(seedA, seedB) {
-    let genA = generator(seedA, 16807), genB = generator(seedB, 48271);
+function main(seedA, seedB, mulA, mulB, limit) {
+    let genA = generator(seedA, 16807, mulA), genB = generator(seedB, 48271, mulB);
     let count = 0
-    for(let i = 0; i < 40000000; i++) {
+    for(let i = 0; i < limit; i++) {
     	if(judge(genA.next().value, genB.next().value)){
     		count++;
     	}
     }
     return count;
 }
+
+main(65, 8921, 1, 1, 40000000); //test star 1
+main(591, 393, 1, 1, 40000000); //main star 1
+main(65, 8921, 4, 8, 5000000); //test star 2
+main(591, 393, 4, 8, 5000000); //main star 2
