14e0e5b874f86ba2f17627bbb8d2acd3c9d306cd
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2017-12-16 16:38:37 +0100

    Day 16 second star (slow solution)

--- day16.js
+++ day16.js
@@ -1,27 +1,37 @@
 var programs = 'abcdefghijklmnop';
+var cycle = 1;
 
-function main(input, programs) {
+function main(input, programs, times) {
     let instructions = input.split(',');
-    for(let instruction of instructions) {
-       programs =  programs[instruction[0]](...instruction.substr(1).split('/'));
+    for (let i = 0; i < times; i++) {
+        for (let instruction of instructions) {
+            programs = programs[instruction[0]](...instruction.substr(1).split('/'));
+        }
     }
     return programs;
 }
 
-Array.prototype.s = function swap (startIndex) {
+Array.prototype.s = function swap(startIndex) {
     let index = Number(startIndex);
-    return [...(this.slice(-index)), ...(this.slice(0, this.length -index))];
+    return [...(this.slice(-index)), ...(this.slice(0, this.length - index))];
 }
 
-Array.prototype.x = function exchange (firstIndex, secondIndex) {
+Array.prototype.x = function exchange(firstIndex, secondIndex) {
     let copy = this.slice();
     [copy[firstIndex], copy[secondIndex]] = [copy[secondIndex], copy[firstIndex]];
     return copy;
 }
 
-Array.prototype.p = function partner (nameA, nameB) {
+Array.prototype.p = function partner(nameA, nameB) {
     let indexA = this.indexOf(nameA), indexB = this.indexOf(nameB);
     return this.x(indexA, indexB);
 }
 
-console.log(main(document.body.firstChild.innerChild.trim(), programs.split(''))).join('');
+var current = main(document.body.firstChild.innerText.trim(), programs.split(''), 1).join('');
+
+while (current !== programs) {
+    current = main(document.body.firstChild.innerText.trim(), current.split(''), 1).join('');
+    cycle++;
+}
+
+console.log(main(document.body.firstChild.innerText.trim(), programs.split(''), (1000000000 % cycle)).join(''));
