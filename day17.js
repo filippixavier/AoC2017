683fa0dfca98b37975f25713c060f11263e0ed0e
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2017-12-17 18:00:49 +0100

    Day 17 second star

--- day17.js
+++ day17.js
@@ -1,11 +1,25 @@
-function main(steps) {
+function after2017(steps, limit) {
     let buffer = [0];
     let index = 0;
-    for(let i = 1; i < 2018; i++) {
+    for(let i = 1; i <= limit; i++) {
         index = (index + steps) % buffer.length;
         buffer.splice(++index, 0, i);
     }
-    return {index, buffer, next: buffer[index+1]};
+    return buffer[index+1];
 }
 
-console.log(main(304));
+function after0(steps, limit) {
+    let index = 0, size = 1, value = 0;
+    for(let i = 1; i <= limit; i++) {
+        index = ((index + steps) % size)+1;
+        size++;
+        if((index - 1) === 0) {
+            value = i;
+        }
+    }
+
+    return value;
+}
+
+console.log(after2017(304, 2017));
+console.log(after0(304, 50000000));
